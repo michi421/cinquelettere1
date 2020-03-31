c4ea66b6fff1149ce7c7322b9c6a787550665d71
spartacus04 <52862005+spartacus04@users.noreply.github.com>
2020-03-31 17:23:16 +0200

    first commit

---
+++ test.js
@@ -0,0 +1,80 @@
+var imagenames = ["befana", "camino", "carota", "denaro", "divano", "fanale", "farina", "ferita", "gelato", "lumaca", "manica", "matita", "mulino", "nuvola", "pecora", "ridere", "salame", "sedano", "sirena", "tavolo"];
+var lastrng = 0;
+var imagerng;
+var correct;
+
+
+function UrlExists(url)
+{
+    var http = new XMLHttpRequest();
+    http.open('HEAD', url, false);
+    http.send();
+    return http.status!=404;
+}
+
+function newimage() {
+    while (true) {
+        imagerng = Math.floor(Math.random() * 100);
+
+        if (imagerng <= 13) {
+            if (imagerng != 0) {
+                if (imagerng != lastrng) {
+                    if (imagerng != undefined) {
+                        lastrng = imagerng;
+                        var urltocheck = './res/images/' + imagenames[imagerng] + '.png'
+                        if(UrlExists(urltocheck)){
+                            document.getElementById('image').setAttribute("src", urltocheck);
+                        }
+                        else {
+                            urltocheck = './res/images/' + imagenames[imagerng] + '.gif'
+                            document.getElementById('image').setAttribute("src", urltocheck);
+                        }
+                        break;
+                    }
+                }
+            }
+        }
+    }
+}
+
+function check() {
+    if (document.getElementById('text').value.length == 5) {
+        document.getElementById('text').setAttribute("readonly", true);
+        if (document.getElementById('text').value.toLowerCase() == imagenames[imagerng]) {
+            correct = true;
+        } else {
+            correct = false;
+        }
+        setTimeout(hidecontrols, 3000);
+    }
+}
+
+var hidecontrols = function () {
+
+    if (correct) {
+        document.getElementById('emo').setAttribute("src", './res/smiley/smile' + imagerng + '.jpg');
+        console.log("si");
+        newimage();
+
+    } else {
+        document.getElementById('emo').setAttribute("src", './res/ops.JPG');
+        console.log("no")
+    }
+
+
+    document.getElementById('image').style.visibility = "hidden";
+    document.getElementById('text').style.visibility = "hidden";
+    document.getElementById('emo').style.visibility = "visible";
+    document.getElementById('text').value = null;
+
+    setTimeout(showcontrols, 3000);
+}
+
+var showcontrols = function () {
+    document.getElementById('image').style.visibility = "visible";
+    document.getElementById('text').style.visibility = "visible";
+    document.getElementById('emo').style.visibility = "hidden";
+    document.getElementById('text').focus();
+    document.getElementById('text').removeAttribute("readonly")
+
+}
